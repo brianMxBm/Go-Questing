1955a69a0c4e973bbcce1874be860067a2ee4b5d
officialsmp305 <Officialsmp305@gmail.com>
2022-06-29 18:07:19 -0700

    [FRONTEND}: Theme Config.

---
+++ theme/colors.tsx
@@ -0,0 +1,8 @@
+/* Maintain all constant color pallates here */
+export default {
+  tabs: '#90D8FA', // Tab nav color
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#808080',
+  lightBlue: '#d4eefa'
+};
