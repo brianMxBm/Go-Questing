73f0f9742ae9f1125405d07ab1bab8b58a470650
officialsmp305 <Officialsmp305@gmail.com>
2022-07-02 14:21:44 -0700

    [FIX]: Transfer Over To Backend Repo.

--- backend/database/index.tsx
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose
-  .connect('mongodb://localhost:27017/test', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(console.log('connected'))
-  .catch((err) => console.log(err));
