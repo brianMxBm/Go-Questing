6dfa24c79bebf1d084179e4275c80f4903d21063
officialsmp305 <Officialsmp305@gmail.com>
2022-07-08 19:48:17 -0700

    [FRONTEND]: Root Navigation

--- App.tsx
+++ App.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import LoginScreen from './assets/screens/LoginScreen';
-import RegisterScreen from './assets/screens/RegisterScreen';
-export default function App() {
-  return (
-    <>
-      <View style={styles.container}>
-        <RegisterScreen />
-        <StatusBar style="auto" />
-      </View>
-    </>
-  );
+import RootNav from './navigation/RootNav';
+
+function App() {
+  return <RootNav />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+export default App;
---
+++ navigation/RootNav.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import HomeScreen from '../assets/screens/HomeScreen';
+import MapScreen from '../assets/screens/MapScreen';
+import ProfileScreen from '../assets/screens/ProfileScreen';
+import FeedScreen from '../assets/screens/FeedScreen';
+import TabNavigation from './TabNavigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function RootStackNavigator() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen name="Tabs" component={TabNavigation} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Feed" component={FeedScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
