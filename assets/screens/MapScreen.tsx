26c493e131213c6b3942740c722a60e757d9fb8c
officialsmp305 <Officialsmp305@gmail.com>
2022-07-26 04:08:18 -0700

    [FRONTEND]: MapScreen Integrated W/ Redux.

--- assets/screens/MapScreen.tsx
+++ assets/screens/MapScreen.tsx
@@ -1,21 +1,59 @@
-import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import mapStyle from '../../theme/mapStyle';
+import { WIN_WIDTH, HEIGHT } from '../../constants/dimensions';
+import { getUserLocation } from '../../redux/actions/locationAction';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black'
+    alignItems: 'center'
+  },
+  mapView: {
+    width: WIN_WIDTH,
+    height: '100%'
+  },
+  centerContainer: {
+    position: 'absolute',
+    paddingVertical: HEIGHT * 0.06,
+    paddingHorizontal: 15,
+    right: 0
   }
 });
 
-function MapScreen() {
+export default function MapScreen() {
+  const dispatch = useAppDispatch();
+  const { location } = useAppSelector((state) => state.location);
+
+  useEffect(() => {
+    if (location.latitude == 0) {
+      //TODO: Change To Null
+      dispatch(getUserLocation());
+    }
+  }, [location, dispatch]);
+
+  if (!location) {
+    return <Text>Loading..</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={{ color: 'white' }}>Test Screen</Text>
+    <View>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.mapView}
+        showsMyLocationButton={false}
+        showsUserLocation
+        customMapStyle={mapStyle}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0015,
+          longitudeDelta: 0.0121
+        }}
+      />
     </View>
   );
 }
-
-export default MapScreen;
