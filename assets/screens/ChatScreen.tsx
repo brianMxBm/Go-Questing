c8ee1ffdfb373cb0c1b9e3071552a7b9d979fccf
officialsmp305 <Officialsmp305@gmail.com>
2022-08-11 01:14:58 -0700

    [FRONTEND]: Add Chat Screen

---
+++ assets/screens/ChatScreen.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black'
+  }
+});
+
+function ChatScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: 'white' }}>Chat Screen</Text>
+    </View>
+  );
+}
+
+export default ChatScreen;
