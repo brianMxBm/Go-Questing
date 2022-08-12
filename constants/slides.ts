5da33ddb8c4b61d6606baf4112b40c9e71e18375
officialsmp305 <Officialsmp305@gmail.com>
2022-08-12 00:44:40 -0700

    [FRONTEND]: Slides & Colors For onBoardingScreen

---
+++ constants/slides.ts
@@ -0,0 +1,31 @@
+import colors from '../theme/colors';
+
+export const DATA = [
+  {
+    key: '1',
+    title: 'Take On SideQuests!',
+    description: 'Go-Quest allows you to experience brand new ways to find gigs near you!',
+    image: 'https://cdn-icons-png.flaticon.com/512/6858/6858154.png'
+  },
+  {
+    key: '2',
+    title: 'Take On SideQuests!',
+    description: 'Go-Quest allows you to experience brand new ways to find gigs near you!',
+    image:
+      'https://cdn-icons.flaticon.com/png/512/4176/premium/4176399.png?token=exp=1660284240~hmac=08ce3024a5e1c146ed15c8dd2b2cced6'
+  },
+  {
+    key: '3',
+    title: 'Take On SideQuests!',
+    description: 'Go-Quest allows you to experience brand new ways to find gigs near you!',
+    image: 'https://cdn-icons-png.flaticon.com/512/7452/7452366.png'
+  },
+  {
+    key: '4',
+    title: 'Take On SideQuests!',
+    description: 'Go-Quest allows you to experience brand new ways to find gigs near you!',
+    image: 'https://cdn-icons-png.flaticon.com/512/387/387413.png'
+  }
+];
+
+export const bg = [colors.tutorialBG, colors.tutorialBG1, colors.tutorialBG2, colors.tutorialBG3];
--- theme/colors.tsx
+++ theme/colors.tsx
@@ -6,5 +6,11 @@ export default {
   gray: '#808080',
   lightBlue: '#d4eefa',
   buttons: '#a5fa90',
-  links: '#6ba7f5'
+  links: '#6ba7f5',
+  errors: '#ee5e65',
+  confirm: '#b1d8a8',
+  tutorialBG: '#77aeff',
+  tutorialBG1: '#ae67fa',
+  tutorialBG2: '#fd315d',
+  tutorialBG3: '#90CB4A'
 };
