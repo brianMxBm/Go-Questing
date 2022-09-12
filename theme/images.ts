2adaff0b53db1d50cd47569f1611575f05d399e8
officialsmp305 <Officialsmp305@gmail.com>
2022-09-12 02:27:20 -0700

    [TEMP]: Add Temporary Image.

--- theme/images.ts
+++ theme/images.ts
@@ -3,3 +3,4 @@ export const compassIcon = require('../assets/images/icons/compassIcon.png');
 export const mapIcon = require('../assets/images/icons/mapIcon.png');
 export const profileIcon = require('../assets/images/icons/profileIcon.png');
 export const messageIcon = require('../assets/images/icons/messageIcon.png');
+export const profilePicture = require('../assets/images/icons/profilePicture.png');
