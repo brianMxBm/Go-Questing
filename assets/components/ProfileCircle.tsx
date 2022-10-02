c6ab77d714f87b2e800593f84c8ead50b62b6f7b
officialsmp305 <Officialsmp305@gmail.com>
2022-10-02 00:12:10 -0700

    [FRONTEND]: Profile Picture Component

---
+++ assets/components/ProfileCircle.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { View, Image, Text, ImageResizeMode, ImageSourcePropType, ImageStyle } from 'react-native';
+
+interface ProfileCircleType {
+  isPicture: boolean;
+  URLPicture?: string;
+  resizeMode?: ImageResizeMode;
+  requirePicture: ImageSourcePropType;
+  userName?: string;
+  width?: number;
+  height: number;
+  shape: string;
+  userContainerStyle?: any;
+  pictureStyle?: ImageStyle;
+}
+
+export default function ProfileCircle({
+  isPicture,
+  shape,
+  pictureStyle,
+  requirePicture,
+  URLPicture,
+  resizeMode,
+  userName,
+  width,
+  height
+}: ProfileCircleType) {
+  const getUserInitial = () => {
+    const firstLetters: string[] = [];
+    const words = userName!.split(' ');
+    words.map((word) => firstLetters.push(word[0]));
+    return firstLetters.map((firstLetter) => firstLetter);
+  };
+
+  if (isPicture) {
+    return (
+      <Image
+        source={requirePicture !== null ? requirePicture : { uri: URLPicture }}
+        style={[
+          { width, height, resizeMode: resizeMode },
+          shape === 'circle' && { borderRadius: 100 },
+          shape === 'rounded' && { borderRadius: 15 },
+          pictureStyle
+        ]}
+      />
+    );
+  } else {
+    return (
+      <View
+        style={[
+          shape === 'circle' && { borderRadius: 100 },
+          shape === 'rounded' && { borderRadius: 15 },
+          {
+            width,
+            height,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }
+        ]}>
+        {userName && <Text style={[{ fontSize: 16 }]}>{getUserInitial()}</Text>}
+      </View>
+    );
+  }
+}
