d12369df08eeb81c15f99eef1b854c90a84d1304
katalla1 <katalla@hotmail.com>
2022-10-13 13:36:41 -0700

    run the commented code on feedscreen once

--- assets/screens/JobFeedScreen.tsx
+++ assets/screens/JobFeedScreen.tsx
@@ -15,10 +15,66 @@ const styles = StyleSheet.create({
     backgroundColor: colors.black
   }
 });
+// interface jobPost {
+//   postTitle: string;
+//   description: string;
+//   compensation: string;
+//   address: string;
+//   jobCategory: string;
+//   latitude: string;
+//   longitude: string;
+// }
+
+// // this function will create the fake data in the database for jobs which can be displayed as markers on the map.
+// // only run this function once then comment out.
+// function getTitle(cat: string) {
+//   switch (cat) {
+//     case 'auto repair':
+//       return 'fix my car';
+//     case 'yard work':
+//       return 'mow my lawn';
+//     case 'plumbing':
+//       return 'unclog a drain';
+//     case 'paint':
+//       return 'paint my house';
+//     default:
+//       return '';
+//   }
+// }
+
+// async function superMarioFaker() {
+//   for (let i = 0; i < 25; ++i) {
+//     const tempCategory = faker.helpers.arrayElement([
+//       'auto repair',
+//       'yard work',
+//       'plumbing',
+//       'paint'
+//     ]);
+//     const tempTitle = getTitle(tempCategory);
+//     const values: jobPost = {
+//       postTitle: tempTitle,
+//       description: faker.lorem.lines(),
+//       compensation: faker.commerce.price(),
+//       jobCategory: tempCategory,
+//       latitude: faker.address.latitude(34.257276, 34.228291, 6),
+//       longitude: faker.address.longitude(-118.493565, -118.562101, 6),
+//       address: ''
+//     };
+//     const response = await fetch(
+//       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${values.latitude},${values.longitude}&key=YOUR_KEY_HERE` <----------add a key to this part
+//     );
+
+//     const data = await response.json();
+//     values.address = data.results[0].formatted_address;
+//     const res = await postJob(values);
+//     console.log(values);
+//     console.log(res);
+//   }
+// }
 
 export default function JobFeedScreen({ navigation }: navigationType) {
   const [selectedTab, setSelectedTab] = useState(jobCategories[0]);
-
+  //superMarioFaker()
   return (
     <ScrollView>
       <SafeAreaView style={styles.container}>
