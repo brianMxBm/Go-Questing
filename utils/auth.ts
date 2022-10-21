fc76019d5a9c594ffc1febf8593bebb1ccb5850e
officialsmp305 <Officialsmp305@gmail.com>
2022-10-21 01:57:05 -0700

    [CONFIG]: Types For Endpoint Requests.

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,35 +1,57 @@
+import axios from 'axios';
 import client from '../api/client';
 
-const catchError = (error: { response: any; message: any; data: any }) => {
+type registerReq = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+type loginReq = {
+  email: string;
+  password: string;
+};
+
+const catchError = (error: { response: any; message: string; data: any }) => {
+  //TODO: Can't stay as any.
   if (error?.response?.data) {
     return error.response.data;
   }
   return { success: false, error: error.message };
 };
 
-export const signUp = async (values: any) => {
+//TODO: Create a request function to unify all of these functions.
+//TODO: Give Errors a type with Axios. Refactor CatchError to accept that.
+export const signUp = async (request: registerReq) => {
   try {
-    const { data } = await client.post('/user/create', { ...values });
+    const { data } = await client.post(
+      '/user/create',
+      { ...request },
+      { timeout: 10000, timeoutErrorMessage: 'Time Out. Please Try Again' }
+    ); //TODO: Temp timeout time, stress test later to get accurate number.
     return data;
   } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+    } else {
+      console.log('Register Failed'); //TODO: implement actual error handling.
+    }
     return catchError(error);
   }
 };
 
-export const signIn = async (values: any) => {
+export const signIn = async (request: loginReq) => {
   try {
-    const { data } = await client.post('/user/signin', { ...values });
+    const { data } = await client.post(
+      '/user/signin',
+      { ...request },
+      { timeout: 10000, timeoutErrorMessage: 'Time Out. Please Try Again' }
+    ); //TODO: Temp timeout time, stress test later to get accurate number.
     return data;
   } catch (error: any) {
-    return catchError(error);
-  }
-};
-
-export const forgotPassword = async (values: any) => {
-  try {
-    const { data } = await client.post('/user/forgot-password', { ...values });
-    return data;
-  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+    } else {
+      console.log('Login Failed'); //TODO: implement actual error handling.
+    }
     return catchError(error);
   }
 };
